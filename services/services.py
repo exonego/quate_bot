afc141abdd84b471ca2f4dc4d8ad9e8217b6cbfc
Pavel R <334554.rud@gmail.com>
2025-08-19 09:51:54 +0300

    helpful func

--- services/services.py
+++ services/services.py
@@ -1,7 +1,14 @@
 from random import choice
-from lexicon.quates import QUATES_RU
 
 
 # That function choice random quate
 def choice_quate() -> str:
-    return choice(QUATES_RU)
+    return choice()
+
+
+# That function return dict of quates
+def read_quates(path: str) -> dict[int, str]:
+    text_file = open(path, encoding="utf-8")
+    enum_quates = enumerate(map(str.rstrip, text_file.readlines()), start=1)
+    text_file.close()
+    return dict(enum_quates)
