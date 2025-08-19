6dbfd06c17e1e96830b77b3ab48075af8954916d
Pavel R <334554.rud@gmail.com>
2025-08-19 09:29:26 +0300

    little bugfix

--- keyboards/quates_kb.py
+++ keyboards/quates_kb.py
@@ -3,10 +3,10 @@ from lexicon.lexicon import LEXICON_RU
 
 
 quate_kb = InlineKeyboardMarkup(
-    *[
+    inline_keyboard=[
         [
             InlineKeyboardButton(
-                text=LEXICON_RU["kb_quate_like"], callback_data="kb_quote_like"
+                text=LEXICON_RU["kb_quate_like"], callback_data="kb_quate_like"
             ),
             InlineKeyboardButton(
                 text=LEXICON_RU["kb_quate_send"], callback_data="kb_quate_send"
