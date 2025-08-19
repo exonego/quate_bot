80bd201ce5eeb22c3c47bd0597c988b897b2212d
Pavel R <334554.rud@gmail.com>
2025-08-19 11:01:51 +0300

    handlers finished

--- handlers/user.py
+++ handlers/user.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+import re
 
 from aiogram import F, Router
 from aiogram.filters import Command, CommandStart
@@ -41,13 +42,14 @@ async def process_favorites_command(message: Message, db: dict, quates: dict):
         await message.answer(
             text=LEXICON_RU[message.text],
             reply_markup=create_favorites_keyboard(
-                quates, *sorted(db["users"][message.from_user.id]["favorites"])
+                quates, *db["users"][message.from_user.id]["favorites"]
             ),
         )
     else:
         await message.answer(text=LEXICON_RU["no_favorites"])
 
 
+# this handler will react when user likes a quate
 @user_router.callback_query(F.data == "kb_quate_like")
 async def process_quate_like(callback: CallbackQuery, db: dict):
     db["users"][callback.from_user.id]["favorites"].add(
@@ -56,9 +58,49 @@ async def process_quate_like(callback: CallbackQuery, db: dict):
     await callback.answer("Цитата добавлена в избранные!")
 
 
+# this handler will react when user doesn't like a quate
 @user_router.callback_query(F.data == "kb_quate_send")
 async def process_quate_send(callback: CallbackQuery, db: dict, quates: dict):
     quate_num = choice_quate(quates)
     db["users"][callback.from_user.id]["cur_quate"] = quate_num
     await callback.message.edit_text(text=quates[quate_num], reply_markup=quate_kb)
     await callback.answer("Новая цитата!")
+
+
+# this handler will react when user click to quate in favorites
+@user_router.callback_query(F.data.isdigit())
+async def process_click_quate(callback: CallbackQuery, db: dict, quates: dict):
+    await callback.message.answer(quates[int(callback.data)])
+
+
+# this handler will react when user want to edit favorites
+@user_router.callback_query(F.data == "edit_favorites")
+async def process_edit_favorites(callback: CallbackQuery, db: dict, quates: dict):
+    await callback.message.edit_text(
+        text=LEXICON_RU["edit_favorites"],
+        reply_markup=create_edit_keyboard(
+            quates, *db["users"][callback.from_user.id]["favorites"]
+        ),
+    )
+
+
+# this handler will react when usser want to delete favorite quate
+@user_router.callback_query(F.data[-3:] == "del")
+async def process_delete_favorite(callback: CallbackQuery, db: dict, quates: dict):
+    db["users"][callback.from_user.id]["favorites"].remove(int(callback.data[:-3]))
+
+    if db["users"][callback.from_user.id]["favorites"]:
+        await callback.message.edit_text(
+            text=LEXICON_RU["edit_favorites"],
+            reply_markup=create_edit_keyboard(
+                quates, *db["users"][callback.from_user.id]["favorites"]
+            ),
+        )
+    else:
+        await callback.message.edit_text(text=LEXICON_RU["no-favorites"])
+
+
+# this handler will react when user click cancel button in favorites(view and edit)
+@user_router.callback_query(F.data == "cancel")
+async def process_cancel_click(callback: CallbackQuery):
+    await callback.message.answer(text=LEXICON_RU["cancel_text"])
